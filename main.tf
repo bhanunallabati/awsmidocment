388325cb5228702bdbd4fe6b71db92b25e141507
bhanunallabati <78374021+bhanunallabati@users.noreply.github.com>
2025-01-06 21:52:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
 provider "aws" {
 }
 resource "aws_instance" "example_server" {
