dff829220d55d43b3e7186da464c9ef0ed615d6e
bhanunallabati <78374021+bhanunallabati@users.noreply.github.com>
2025-01-06 22:04:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 }
 resource "aws_instance" "example_server" {
-  ami           = "ami-04e914639d0cca79a"
+  ami           = "ami-0b4624933067d393a"
   instance_type = "t2.micro"
   tags = {
     Name = "JacksBlogExample"
